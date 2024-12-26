4c4803eae17d0911c486149ad1e8f60c5c9832d1
olegmir59 <udom1@ya.ru>
2024-12-27 03:02:42 +0700

    Add file

--- module_16_1.py
+++ module_16_1.py
@@ -3,24 +3,23 @@ from fastapi import FastAPI
 app = FastAPI()
 # Определение базового маршрута
 @app.get("/")
-async def read_root() -> dict:
+async def get_root_page() -> dict:
     return {"message": "Главная страница"}
 
 
 @app.get("/user/admin")
-async def get_admin():
+async def get_admin_page():
     return {"message": "Вы вошли как администратор"}
 
 @app.get("/user/{user_id}")
-async def user_from_id(user_id: int) -> dict:
+async def get_user_id(user_id: int) -> dict:
     return {"message": f"Вошли как пользователь № {user_id}"}
 
 @app.get("/user/")
-async def user_info(user_id=None) -> dict:
+async def get_user_info(user_id=None) -> dict:
     if user_id is None:
         return {"message": "Информация о пользователе. Имя: <username>, Возраст: <age>."}
     else:
         # Здесь мог бы быть дополнительный код для обработки ситуации,
         # когда `user_id` передан, но он отсутствует в базе.
         return {"message": f"Информация о пользователе. Имя: <username>, Возраст: <age>."}
-
